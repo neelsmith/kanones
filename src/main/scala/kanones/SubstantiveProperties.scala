16f115c025eb76e8fc25bf797ae2734b771de82d
Neel Smith <dnsmith.neel@gmail.com>
2017-08-09 10:54:48 -0400

    Add substantive properties

--- src/main/scala/kanones/SubstantiveProperties.scala
+++ src/main/scala/kanones/SubstantiveProperties.scala
@@ -8,3 +8,18 @@ case object Genitive extends GrammaticalCase
 case object Dative extends GrammaticalCase
 case object Accusative extends GrammaticalCase
 case object Vocative extends GrammaticalCase
+
+
+sealed trait Gender
+
+case object Masculine extends Gender
+case object Feminine extends Gender
+case object Neuter extends Gender
+
+
+
+sealed trait GrammaticalNumber
+
+case object Singular extends GrammaticalNumber
+case object Dual extends GrammaticalNumber
+case object Plural extends GrammaticalNumber
---
+++ src/test/scala/kanones/SubstantivePropertiesSpec.scala
@@ -0,0 +1,43 @@
+
+package edu.holycross.shot.kanones
+
+import org.scalatest.FlatSpec
+
+class SustantivePropertiesSpec extends FlatSpec {
+
+
+  "The  GrammaticalCase trait" should "include all Greek cases" in {
+
+    val nominativeExample : GrammaticalCase = Nominative
+    val isNominative = nominativeExample match {
+      case Nominative => true
+      case Genitive => false
+      case Dative => false
+      case Accusative => false
+      case Vocative => false
+    }
+    assert(isNominative)
+  }
+
+
+  "The Gender trait" should "include all Greek genders" in {
+    val neuterExample: Gender = Neuter
+    val isNeuter = neuterExample match {
+      case Masculine => false
+      case Feminine => false
+      case Neuter => true
+    }
+    assert(isNeuter)
+  }
+
+
+  "The GrammaticalNumber trait" should "include all Greek numbers" in {
+    val dualExample: GrammaticalNumber = Dual
+    val isDual = dualExample match {
+      case Singular => false
+      case Plural => false
+      case Dual => true
+    }
+    assert(isDual)
+  }
+}
