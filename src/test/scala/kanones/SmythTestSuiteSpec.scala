39ae3ee3f0aca631565b3826b529266c1f318ebe
Neel Smith <dnsmith.neel@gmail.com>
2017-08-18 16:53:34 -0400

    Add Smyth TestSuite

---
+++ src/test/scala/kanones/SmythTestSuiteSpec.scala
@@ -0,0 +1,32 @@
+
+package edu.holycross.shot.kanones
+
+import org.scalatest.FlatSpec
+import java.io.File
+
+class SmythTestSuiteSpec extends FlatSpec {
+
+  val conf = TestConfig("/bin/echo", "/usr/local/bin/fst-infl", "parsers/dev/greek.a")
+  val testHarness = SimpleTestHarness(conf)
+
+
+  "A SimpleTestHarness" should "score files one at a time" in {
+    val testSrc = new File("src/test/resources/unit_tests_data/smyth")
+    val subDirs =  testSrc.listFiles().toVector.filter(_.isDirectory())
+
+
+    val scores = StringBuilder.newBuilder
+    for (dir <- subDirs) {
+      scores.append("\n" + dir.getName() + "\n")
+      val cex = dir.listFiles().toVector.filter(_.getName().contains(".cex"))
+      for (testFile <- cex) {
+        val score = testHarness.score(testFile)
+        scores.append("\t" + testFile.getName() + ": " + score.filter(_ == true).size + "/" + score.size + "\n")
+      }
+
+    }
+    println(scores.toString)
+
+  }
+
+}
