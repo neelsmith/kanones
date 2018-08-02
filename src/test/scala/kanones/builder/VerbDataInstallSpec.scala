ec7d4ab707538ffc6aae2411a9ea09184bde668c
Neel Smith <dnsmith.neel@gmail.com>
2018-08-02 12:31:32 -0400

    Test verb data install

--- src/test/scala/kanones/builder/VerbDataInstallSpec.scala
+++ src/test/scala/kanones/builder/VerbDataInstallSpec.scala
@@ -1,6 +1,9 @@
 package edu.holycross.shot.kanones.builder
 
 import org.scalatest.FlatSpec
+import better.files._
+import java.io.{File => JFile}
+import better.files.Dsl._
 
 class VerbDataInstallSpec extends FlatSpec {
 
@@ -15,5 +18,30 @@ class VerbDataInstallSpec extends FlatSpec {
         assert(t.toString.startsWith(beginning))
       }
     }
+  }
+
+  it should "compose FST for a valid CEX string" in {
+    val goodLine = "lverbinfl.are_presind1#conj1#o#1st#sg#pres#indic#act"
+    val goodFst = VerbRulesInstaller.verbRuleToFst(goodLine)
+    val expected = "<conj1><verb>o<1st><sg><pres><indic><act><u>lverbinfl\\.are\\_presind1</u>"
+    assert(goodFst.trim ==  expected)
+  }
+
+  it should "compose FST from CEX files in a directory" in {
+    val repo = File(".")
+    val corpusName = "testCorpus"
+    val verbDataDir = mkdirs(repo/"datasets"/corpusName/"rules-tables/verbs")
+    val goodLine = "lverbinfl.are_presind1#conj1#o#1st#sg#pres#indic#act"
+    val cexFile = verbDataDir/"madeuptestdata.cex"
+    cexFile.overwrite(goodLine)
+
+    val fstFromDir = VerbRulesInstaller.fstForVerbRules(verbDataDir)
+
+    val lines = fstFromDir.split("\n").toVector
+    val expected = "$verbinfl$ =  <conj1><verb>o<1st><sg><pres><indic><act><u>lverbinfl\\.are\\_presind1</u>"
+    // tidy up
+    (repo/"datasets"/corpusName).delete()
+
+    lines(0) == expected
   }
 }
