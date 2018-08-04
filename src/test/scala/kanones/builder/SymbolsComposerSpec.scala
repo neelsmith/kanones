408c9f67edb27696c64c397fbf50adeb3d43bf9c
Neel Smith <dnsmith.neel@gmail.com>
2018-08-04 16:09:55 -0400

    Test symbols composer

--- src/test/scala/kanones/builder/SymbolsComposerSpec.scala
+++ src/test/scala/kanones/builder/SymbolsComposerSpec.scala
@@ -8,10 +8,24 @@ import java.io.{File => JFile}
 
 class SymbolsComposerSpec extends FlatSpec {
 
-  "The SymbolsComposer object" should "install files" in {
+  val repo = File(".")
+  val corpus = "minimal"
 
-      val repo = File(".")
-      val corpus = "minimal"
+
+  "The SymbolsComposer object" should "compose the main symbols.fst file" in {
+    SymbolsComposer.composeMainFile(repo/"parsers"/corpus)
+    val expectedFile = repo/"parsers"/corpus/"symbols.fst"
+    val written  = expectedFile.exists()
+    // tidy up
+    (repo/"parsers"/corpus).delete()
+    assert(written)
+  }
+
+  it should "copy files from source to build area" in pending
+
+  it should "rewrite phonology files" in pending
+
+  it should "install correct set of files from source" in pending /*{
       SymbolsComposer(repo, corpus)
 
       val symbolsDir = repo/"parsers"/corpus/"symbols"
@@ -26,6 +40,5 @@ class SymbolsComposerSpec extends FlatSpec {
       //tidy up
       (repo/"parsers"/corpus).delete()
       assert(fstFiles == expectedFiles)
-
-  }
+  }*/
 }
