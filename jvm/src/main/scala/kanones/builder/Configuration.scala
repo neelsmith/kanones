c246c53b3169b1cb348f892702f074d14f293337
Neel Smith <dnsmith.neel@gmail.com>
2020-09-02 15:42:32 -0400

    Begin porting higher-level structures from tabulae

---
+++ jvm/src/main/scala/kanones/builder/Configuration.scala
@@ -0,0 +1,43 @@
+package edu.holycross.shot.kanones.builder
+
+import better.files.{File => ScalaFile, _}
+import better.files.Dsl._
+
+
+import wvlet.log._
+import wvlet.log.LogFormatter.SourceCodeLogFormatter
+
+/** Required configuration information to compile a binary parser.
+*
+* @param fstcompile Path to fst-compiler-utf8
+* @param fstinfl Path to fst-infl
+* @param make Path to make
+* @param datadir Name, as a String, of root directory for data sets.
+*/
+case class Configuration(fstcompile: String, fstinfl: String, make: String, datadir: String = "datasets") extends LogSupport {
+
+  require(ScalaFile(fstcompile).exists(), "No fst compiler named " + fstcompile + " found.")
+  require(ScalaFile(fstinfl).exists(), "No file named " + fstinfl + " found.")
+  require(ScalaFile(make).exists(), "No file named " + make + " found.")
+  require(ScalaFile(datadir).exists(), "No directory named " + datadir + " found.")
+}
+
+
+/** Factory for constructing a [[Configuration]] from a config file.
+*/
+object Configuration {
+
+  /** Create a [[Configuration]] instance from a  configuration file.
+  *
+  * @param config Configuration file.
+  */
+  def apply(config: ScalaFile): Configuration = {
+    val lines = config.lines.toVector.filter(_.nonEmpty).filter(_(0) != '#')
+    val mapped = lines.map( l => {
+      val parts = l.split("=")
+      parts(0).trim -> parts(1).trim
+    }).toMap
+    Configuration(mapped("FSTCOMPILER"),mapped("FSTINFL"),mapped("MAKE"),mapped("datadir"))
+  }
+
+}
---
+++ jvm/src/main/scala/kanones/builder/FstCompiler.scala
@@ -0,0 +1,78 @@
+package edu.holycross.shot.kanones.builder
+
+
+import scala.sys.process._
+
+import better.files.{File => ScalaFile, _}
+import better.files.Dsl._
+
+import wvlet.log._
+import wvlet.log.LogFormatter.SourceCodeLogFormatter
+
+
+object FstCompiler extends LogSupport {
+
+
+  /** Compile lexica, morphological rule sets and core parser logic
+  *
+  */
+  def compileAll(
+    dataSets: ScalaFile,
+    corpusList: Vector[String],
+    parser: ScalaFile,
+    fstSource: ScalaFile,
+    conf: Configuration) : Unit = {
+    // Install data and rules, converting tabular data to FST
+    //println(s"\n\n  Install data for ${corpus} in ${dataDirectory}...")
+    DataInstaller(dataSets, corpusList, parser)
+
+    //println(s"Install rules for ${corpus} in ${dataDirectory}...")
+    //RulesInstaller(dataSets, corpusList, parser, fstSource)
+
+    //println("Compose build")
+    // Compose makefiles and higher-order FST for build system
+    //BuildComposer(dataSets, corpusList, parser, fstSource, conf.fstcompile)
+
+
+    // Build it!
+    val buildDirectory = parser / corpusList.mkString("-")
+    val inflMakefile = buildDirectory / "inflection/makefile"
+    val makeInfl = s"${conf.make} -f ${inflMakefile}"
+    makeInfl !
+
+    val makefile = buildDirectory / "makefile"
+    val doit = s"${conf.make} -f ${makefile}"
+    doit !
+  }
+
+
+  /**  Compose a parser in FST from tabular source data, and compile it to binary form.
+  *
+  * @param dataDirectory Base directory for finding source data from which
+  * the parser will be built.
+  * @param baseDir A writable working directory where the binary parser
+  * will be written.  Specifically, the parser will be written within a subdirectory named `corpus` of a subdirectory named "`parsers`" of
+  * `baseDir`.
+  * @param corpus  Name of "corpus", used as name of subdirectory where
+  * binary parser will be written.
+  * @param conf Configuration object for compiling a parser.
+  */
+  def compile(dataDirectory: ScalaFile,  corpusList: Vector[String], parsers: ScalaFile, fstSrc: ScalaFile, conf: Configuration, replaceExisting: Boolean = true) : Unit = {
+
+    val projectDir = parsers / corpusList.mkString("-")
+    if (projectDir.exists) {
+      replaceExisting match {
+        case true => {
+          projectDir.delete()
+          compileAll(dataDirectory,  corpusList, parsers, fstSrc, conf)
+        }
+        case false => {
+          println("Directory " + projectDir + " exists, and setting to delete exising parser is false.")
+          println("Cowardly refusing to continue.")
+        }
+      }
+    } else {
+      compileAll(dataDirectory, corpusList, parsers, fstSrc, conf)
+    }
+  }
+}
